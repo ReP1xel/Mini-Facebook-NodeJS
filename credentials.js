c9a91a8ea336f1e609c1b0e15263fd90faca341a
ReP1xel <nhgiaanitvn@gmail.com>
2023-02-24 18:14:10 +0700

    Modified credentials.js Deleted init.js

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "connection_string": "mongodb+srv://p1xel:pixel@cluster0.o6gor.mongodb.net/test?authSource=admin&replicaSet=atlas-xbypy6-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+    "connection_string": "mongodb+srv://p1xel:p1xel@cluster0.zo3jbvv.mongodb.net/test",
     "secret_key": "o*oanw8ionm9",
     "client_id": "695711891483-2rqc14kr6esdoah09p6vk42rbc1i0s8n.apps.googleusercontent.com",
     "client_secret": "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",
